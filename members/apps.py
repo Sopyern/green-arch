42ae548a5060329860e360a1f3d510eb8066661f
ABDULHAKIM SIUFYAN BAKO <64008445+Sopyern@users.noreply.github.com>
2022-02-13 10:10:38 +0100

    Update apps.py

--- members/apps.py
+++ members/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class MembersConfig(AppConfig):
     name = 'members'
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'blog'
