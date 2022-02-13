447d31d84f3c9549a35ec3e46cfa92856e3687c5
ABDULHAKIM SIUFYAN BAKO <64008445+Sopyern@users.noreply.github.com>
2022-02-13 10:09:45 +0100

    Update apps.py

--- projects/apps.py
+++ projects/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class ProjectsConfig(AppConfig):
     name = 'projects'
+    
+    
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'blog'
